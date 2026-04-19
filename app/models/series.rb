f674fa64241ab1c98935aefd25f8b2653d06b3a4
davidpacecode <davidpace@gmail.com>
2026-04-18 23:07:06 -0500

    i think i have series basically working

--- app/models/series.rb
+++ app/models/series.rb
@@ -4,6 +4,7 @@ class Series < ApplicationRecord
   belongs_to :bottom_seed, class_name: "Team"
   belongs_to :winner, class_name: "Team", optional: true
 
+  enum :round, { first_round: 0, conference_semifinals: 1, conference_finals: 2, nba_finals: 3 }
   enum :conference, { eastern: 0, western: 1, finals: 2 }
   enum :status, { scheduled: 0, in_progress: 1, complete: 2 }
 end
