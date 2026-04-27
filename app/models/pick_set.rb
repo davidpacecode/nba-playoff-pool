bf979aa4703eda3d8eeff2d420ae6ce9df8359fb
davidpacecode <davidpace@gmail.com>
2026-04-26 22:11:15 -0500

    checking in sunday night - gonna do 2nd round and beyond for bracket then pickset next

--- app/models/pick_set.rb
+++ app/models/pick_set.rb
@@ -2,4 +2,18 @@ class PickSet < ApplicationRecord
   belongs_to :user
   belongs_to :bracket
   has_many :picks
+
+  after_create :create_picks
+
+  private
+
+    def create_picks
+#      { first_round: 4, conference_semifinals: 2, conference_finals: 1 }.each do |round, count|
+#        [:eastern, :western].each do |conference|
+#          count.times { |slot| series.create!(round: round, conference: conference, status: :scheduled, seed_slot: slot) }
+#        end
+#      end
+#
+#      series.create!(round: :nba_finals, conference: :finals, status: :scheduled)
+    end
 end
